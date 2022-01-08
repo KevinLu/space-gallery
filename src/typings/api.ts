33dc9a3d1f01cdbd99d749629ec59204cebb83aa
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2022-01-07 21:39:10 -0500

    feat: use union operator for different APOD image types

--- src/typings/api.ts
+++ src/typings/api.ts
@@ -1,14 +1,25 @@
-// APOD Image
-export type APODImage = {
+type BaseAPODImage = {
   copyright?: string; // defined if image not in public domain
   date: string; // YYYY-MM-DD
   explanation: string;
   hdurl: string;
-  media_type: string;
   service_version: string;
   title: string;
   url: string;
 };
+
+// APOD Image
+export type APODImage = BaseAPODImage &
+  (
+    | {
+        media_type: 'image';
+        thumbnail_url: undefined;
+      }
+    | {
+        media_type: 'video';
+        thumbnail_url: string; // only defined if media_type is video
+      }
+  );
 
 // responses from the API can be a single object or array of APODImage
 export type APODResponse = APODImage | Array<APODImage>;
